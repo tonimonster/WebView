178ef201f796adcf08f200f5064b40c3840a1d38
tonimonster <53837234+tonimonster@users.noreply.github.com>
2019-08-07 01:13:25 +0300

    Initial commit

---
+++ app/src/main/java/web/view/MainActivity.java
@@ -0,0 +1,33 @@
+package web.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.web);
+        webView.loadUrl("http://helpdesk.automaster.od.ua/app/");
+        webView.setWebViewClient(new MyWebViewClient());
+    }
+
+
+    private class MyWebViewClient extends WebViewClient {
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+    }
+}
